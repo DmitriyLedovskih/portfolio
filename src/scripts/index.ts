fe5311c0da1d52787d9175eff90af31359483b51
DmitriyLedovskih <dmitryledovskih1@yandex.ru>
2023-04-28 04:18:59 +0700

    Feat: добавил дату на нижней панели и сделал открытие папки

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,5 +1,5 @@
 import "../styles/index.scss";
-
+// Код пока не очень потом буду улучшать
 const element = document.querySelectorAll(".element");
 
 element.forEach((el) => {
@@ -10,3 +10,66 @@ element.forEach((el) => {
     el.classList.add("element_active");
   });
 });
+
+class Modal {
+  _modal: Element;
+  _button: Element;
+  constructor(modal: Element, button: Element) {
+    this._modal = modal;
+    this._button = button;
+  }
+
+  open() {
+    if (
+      this._modal.getAttribute("data-modal") ===
+      this._button.getAttribute("data-modal-button")
+    ) {
+      this._modal.classList.add("modal_opened");
+    }
+  }
+
+  close() {
+    this._modal.classList.remove("modal_opened");
+  }
+}
+
+const buttons: Element | any = document.querySelectorAll("[data-modal-button]");
+const modals: Element | any = document.querySelectorAll("[data-modal]");
+modals.forEach((modal: Element) => {
+  const closeButton = modal.querySelector(".modal__buttons-button_type_close");
+  const fullScreenButton = modal.querySelector(
+    ".modal__buttons-button_type_full"
+  );
+  buttons.forEach((button: Element) => {
+    button.addEventListener("dblclick", () => {
+      new Modal(modal, button).open();
+    });
+    closeButton?.addEventListener("click", () => {
+      new Modal(modal, button).close();
+    });
+    fullScreenButton?.addEventListener("click", () => {
+      modal.classList.toggle("modal_type_fullscreen");
+    });
+  });
+});
+
+const bottomPanelTime: Element | any = document.querySelector(
+  ".bottom__panel-time"
+);
+
+const bottomPanelDate: Element | any = document.querySelector(
+  ".bottom__panel-date"
+);
+
+setInterval(() => {
+  const time = new Date();
+  bottomPanelTime.textContent = `${time.getHours()}:${
+    time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes()
+  }`;
+}, 1000);
+const date = new Date();
+
+bottomPanelDate.textContent = `${date.getDate()}.0${
+  date.getMonth() + 1
+}.${date.getFullYear()}`;
+console.log(date);
