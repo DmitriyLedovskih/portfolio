890219c8741a5fbc71b14457b699d01553309159
DmitriyLedovskih <dmitryledovskih1@yandex.ru>
2023-06-06 02:09:51 +0700

    Feat: исправил баги добавил новые фичи

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,10 +1,10 @@
 import "../styles/index.scss";
 // Код пока не очень потом буду улучшать
-const element = document.querySelectorAll(".element");
+const element: Element | any = document.querySelectorAll(".element");
 
-element.forEach((el) => {
+element.forEach((el: Element) => {
   el?.addEventListener("click", () => {
-    element.forEach((item) => {
+    element.forEach((item: Element) => {
       item.classList.remove("element_active");
     });
     el.classList.add("element_active");
@@ -20,12 +20,7 @@ class Modal {
   }
 
   open() {
-    if (
-      this._modal.getAttribute("data-modal") ===
-      this._button.getAttribute("data-modal-button")
-    ) {
-      this._modal.classList.add("modal_opened");
-    }
+    this._modal.classList.add("modal_opened");
   }
 
   close() {
@@ -35,20 +30,75 @@ class Modal {
 
 const buttons: Element | any = document.querySelectorAll("[data-modal-button]");
 const modals: Element | any = document.querySelectorAll("[data-modal]");
+const bottomPanelbuttons: Element | any = document.querySelectorAll(
+  "[data-modal-button-bottom-panel]"
+);
 modals.forEach((modal: Element) => {
   const closeButton = modal.querySelector(".modal__buttons-button_type_close");
   const fullScreenButton = modal.querySelector(
     ".modal__buttons-button_type_full"
   );
-  buttons.forEach((button: Element) => {
-    button.addEventListener("dblclick", () => {
-      new Modal(modal, button).open();
+  const rollUpButton = modal.querySelector(
+    ".modal__buttons-button_type_roll-up"
+  );
+  const header: any = modal.querySelector(".modal__header");
+  const content: any = modal.querySelector(".modal__content");
+  bottomPanelbuttons.forEach((but: Element) => {
+    buttons.forEach((button: Element) => {
+      button.addEventListener("dblclick", () => {
+        if (
+          modal.getAttribute("data-modal") ===
+            button.getAttribute("data-modal-button") &&
+          but.getAttribute("data-modal-button-bottom-panel") ===
+            button.getAttribute("data-modal-button")
+        ) {
+          new Modal(modal, button).open();
+          but.classList.add("bottom__panel-button_active");
+        }
+      });
+      closeButton?.addEventListener("click", () => {
+        new Modal(modal, button).close();
+        if (
+          modal.getAttribute("data-modal") ===
+            button.getAttribute("data-modal-button") &&
+          but.getAttribute("data-modal-button-bottom-panel") ===
+            button.getAttribute("data-modal-button")
+        ) {
+          but.classList.remove("bottom__panel-button_active");
+          but.classList.remove("bottom__panel-button_open");
+        }
+      });
+      rollUpButton?.addEventListener("click", () => {
+        if (
+          modal.getAttribute("data-modal") ===
+            button.getAttribute("data-modal-button") &&
+          but.getAttribute("data-modal-button-bottom-panel") ===
+            button.getAttribute("data-modal-button")
+        ) {
+          new Modal(modal, button).close();
+          but.classList.add("bottom__panel-button_open");
+          but.classList.remove("bottom__panel-button_active");
+        }
+      });
+      fullScreenButton?.addEventListener("click", () => {
+        modal.classList.toggle("modal_type_fullscreen");
+        header.classList.toggle("modal_header_type_fullscreen");
+        content.classList.toggle("modal_content_type_fullscreen");
+      });
     });
-    closeButton?.addEventListener("click", () => {
-      new Modal(modal, button).close();
-    });
-    fullScreenButton?.addEventListener("click", () => {
-      modal.classList.toggle("modal_type_fullscreen");
+  });
+});
+
+modals.forEach((modal: Element) => {
+  bottomPanelbuttons.forEach((button: Element) => {
+    button.addEventListener("click", () => {
+      if (
+        modal.getAttribute("data-modal") ===
+        button.getAttribute("data-modal-button-bottom-panel")
+      ) {
+        new Modal(modal, button).open();
+        button.classList.toggle("bottom__panel-button_active");
+      }
     });
   });
 });
@@ -69,7 +119,11 @@ setInterval(() => {
 }, 1000);
 const date = new Date();
 
-bottomPanelDate.textContent = `${date.getDate()}.0${
-  date.getMonth() + 1
-}.${date.getFullYear()}`;
-console.log(date);
+bottomPanelDate.textContent =
+  date.getDate() <= 10
+    ? `0${date.getDate()}.${
+        date.getMonth() + 1 <= 10
+          ? `0${date.getMonth() + 1}`
+          : date.getMonth() + 1
+      }.${date.getFullYear()}`
+    : `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
